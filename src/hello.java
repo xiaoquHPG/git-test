794f0af9b9bd9790da0f9a4363825cd77e38a840
quruiqi <3570946579@qq.com>
2021-12-31 14:23:17 +0800

    hot-fix commit1

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("屈瑞琪");
+        System.out.println("我合并分支了");
+        System.out.println("首先我得提交一次");
+        System.out.println("然后切换回我的master");
     }
 }
